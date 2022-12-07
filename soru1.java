bfe20f662c58c8948687d13df5f18cd2dbe57d6d
yasar12 <40390026+yasar12@users.noreply.github.com>
2022-12-07 18:29:40 +0300

    Add files via upload

---
+++ soru1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+       Scanner a=new Scanner(System.in);
+       System.out.println("Birinci sayıyı giriniz");
+       int b=a.nextInt();
+        System.out.println("Birinci sayıyı giriniz");
+        int c=a.nextInt();
+int d=b+c;
+System.out.println(d);
+
+    }
+}
